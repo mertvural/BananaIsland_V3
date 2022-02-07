e207e6a289bf90fb650a98dd4ad3a5f523305331
mert vural <mertvural90@gmail.com>
2022-02-07 11:53:59 +0300

    test

--- src/js/main.js
+++ src/js/main.js
@@ -573,11 +573,11 @@ var vue = new Vue({
 
         $this.$nextTick(function () {
 
-            document.addEventListener("wheel", function (event) {
-                if (!$this.walkDelay) event.deltaY < 0 ? $this.goMonkey() : $this.backMonkey()
-            });
+            // document.addEventListener("wheel", function (event) {
+            //     if (!$this.walkDelay) event.deltaY < 0 ? $this.goMonkey() : $this.backMonkey()
+            // });
 
-            document.addEventListener("touchmove", function (event) {//touchstart
+            document.addEventListener("touchstart", function (event) {//touchstart
                 if (!$this.walkDelay) $this.goMonkey()
             });
 
