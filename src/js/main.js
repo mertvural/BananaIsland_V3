1c814398e1ded25dc4d6db99b8ac375b06e429c5
mert vural <mertvural90@gmail.com>
2022-02-20 00:55:22 +0300

    test

--- src/js/main.js
+++ src/js/main.js
@@ -790,7 +790,7 @@ var vue = new Vue({
             }, 2500);
         }
 
-        if(!videoActiveNext.attr("autoplay" && $this.isSafari)) {
+        if(!videoActiveNext.attr("autoplay" && $this.isSafari())) {
             alert("safari")
             videoActiveNext[0].play()
             videoActiveNext[0].pause()
