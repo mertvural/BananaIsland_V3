6c3ed79ac8ada6aeb52205b8b5edaec66b241091
mert vural <mertvural90@gmail.com>
2022-02-19 13:03:23 +0300

    test2

--- src/js/main.js
+++ src/js/main.js
@@ -481,9 +481,7 @@ var vue = new Vue({
                 }
                 else {
                     $this.activeScene++            
-                    $(".video-active + .video-capsul video")[0].currentTime = 0
-                    $(".video-active + .video-capsul video")[0].play()
-                    $(".video-active + .video-capsul video")[0].pause()
+                   
                 }
             }
 
